10958b7037682a0317f9df0814316b6eb1d385a4
KresimirSecan <secan.kresimir@gmail.com>
2026-05-18 13:24:09 +0200

    Server dbconnection and plc model made

---
+++ Server/Server/Application/DTOs/PlcDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Server.Application.DTOs
+{
+    public class PlcDto
+    {
+        public string Ip { get; set; }
+        public int Rack { get; set; }
+        public int Slot { get; set; }
+
+        public string Cpu { get; set; }
+
+        public enum CpuType
+        {
+            S7200 = 0,
+            S7300 = 10,
+            S7400 = 20,
+            S71200 = 30,
+            S71500 = 40
+        }
+    }
+}
+
+
---
+++ Server/Server/Application/Interfaces/IServerService.cs
@@ -0,0 +1,9 @@
+﻿using Server.Application.DTOs;
+
+namespace Server.Application.Interfaces
+{
+    public interface IServerService
+    {
+        PlcDto GetPlc();
+    }
+}
