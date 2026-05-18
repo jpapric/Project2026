a3739e54451d2cfedeb99812c210abff4229fd40
KresimirSecan <secan.kresimir@gmail.com>
2026-05-18 10:26:02 +0200

    Add server structure

---
+++ Server/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Serialization;
+//using VIdeoStoreApp.Application.Interfaces;
+//using VIdeoStoreApp.Application.Services;
+//using VIdeoStoreApp.Infrastructure.Repository;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+        
+
+            //builder.Services.AddScoped<IVideoStoreRepository, VideoStoreRepository>();
+            //builder.Services.AddScoped<IVideoStoreService, VideoStoreService>();
+
+            //Dependency injection (DI)
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
