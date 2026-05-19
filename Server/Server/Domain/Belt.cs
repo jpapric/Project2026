200227f31ccf702a6592384663b43c60ddbb439d
Carlsberg <eksplofizzz@gmail.com>
2026-05-19 10:29:22 +0200

    collision

---
+++ Server/Server/Domain/Belt.cs
@@ -0,0 +1,15 @@
+﻿namespace Server.Domain
+{
+    public class Belt
+    {
+        public float Speed { get; }
+        public float Torque { get; }
+        public bool Start_stop { get; }
+        public enum Status
+        {
+            stopped = 0,
+            prepared = 1,
+            running = 2
+        }
+    }
+}
