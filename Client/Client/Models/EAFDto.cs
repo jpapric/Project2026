0a7478060c58e2a9b84878eab7b90e341a816fed
jpapric <jovana.papric2@gmail.com>
2026-05-20 13:07:26 +0200

    add frontend-backend connection baseline

---
+++ Client/Client/Models/EAFDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class EAFDto
+    {
+        public bool Scrap_loading { get; set; }
+        public bool Tapping_active { get; set; }
+        public float Actual_tilting { get; set; }
+        public float Material_weight { get; set; }
+        public float Actual_current { get; set; }
+        public float Energy_consumed { get; set; }
+        public float Actual_temperature { get; set; }
+
+        public bool Furnace_overfill { get; set; }
+        public bool Tapping_error { get; set; }
+        public bool Furnace_empty { get; set; }
+        public bool Furnace_overtemperature { get; set; }
+    }
+}
--- Client/Client/Proxies/EAFProxy.cs
+++ Client/Client/Proxies/EAFProxy.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,18 @@ namespace Client.Proxies
 {
     public class EAFProxy
     {
+        private HttpClient _httpClient;
+
+        public EAFProxy()
+        {
+            string baseUrl = ConfigurationManager.AppSettings["BaseUrl"];
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+        }
+
+
     }
 }
