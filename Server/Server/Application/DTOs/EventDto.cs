e8d4a5940cd3012c08f7dd47f4c0fd04ca3cf961
Carlsberg <eksplofizzz@gmail.com>
2026-05-21 09:28:33 +0200

    Added EventDto

---
+++ Server/Server/Application/DTOs/EventDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Application.DTOs
+{
+    public class EventDto
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
